f0be43c52b452c19958733d5e302022bbe008d50
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-02 13:06:18 +0200

    Comments

--- src/types/DisplayDataProps.ts
+++ src/types/DisplayDataProps.ts
@@ -1,9 +1,10 @@
 interface DisplayDataProps{
-    link: string 
-    icon: string 
+    link: string //link of the site for the account
+    icon: string //base64 of the image or the site + /favicon.ico
     email: string
-    date: string
-    type: string
+    date: string //date the account was created
+    type: string //?type of account
+    id?: string //unique id of the account
 }
 
 export default DisplayDataProps;
