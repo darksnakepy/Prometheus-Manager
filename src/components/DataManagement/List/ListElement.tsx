d10f7b6d01c22e46ad872caacc1d9b7fadd7bb6d
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-02 13:06:56 +0200

    List element component and scrollbar style

--- src/components/DataManagement/List/ListElement.tsx
+++ src/components/DataManagement/List/ListElement.tsx
@@ -3,13 +3,55 @@ import DisplayDataProps from "~/types/DisplayDataProps";
 
 const ListElement = ({date, email, icon, link, type} :DisplayDataProps) => {
     return (
-        <div className="w-[68.5%] bg-slate-400 h-32 ml-[12%] border-2 border-[#1545af] rounded-2xl flex flex-row">
-           <div className="h-16">
-                <Image src="/0yha5ybx1qk71.png" width={64} height={64} alt={""}></Image>
+        <>
+        <div className="w-[68.5%] h-20 ml-[12%] border-2 border-[#1545af] rounded-md flex flex-row relative text-white mb-4">
+           <div className="flex items-center ml-4">
+                <div className="w-16">
+                        <Image src="/0yha5ybx1qk71.png" width={64} height={64} alt={"icon"} className="rounded-2xl"></Image>
+                </div>
            </div>
-           
+           <div className="flex flex-col justify-center ml-5">
+                <div className="font-bold">
+                    {link}
+                </div>
+                <div>
+                    {email}
+                </div>
+           </div>
+           <div className="absolute left-[50%] flex items-center h-full">
+            {date}
+           </div>
         </div>
+        </>
     )
 }
 
 export default ListElement;
+
+/*
+<style>
+  .container {
+    position: relative;
+  }
+
+  .fixed-x {
+    position: absolute;
+    left: 100px; // Adjust this value to set the desired X-axis position
+}
+</style>
+
+<div class="container">
+  <div>
+    Content before the fixed element.
+  </div>
+
+  <div class="fixed-x">
+    This element is fixed at X = 100px, regardless of previous content length.
+  </div>
+</div>
+
+<div>
+  More content after the fixed element.
+</div>
+
+ */
