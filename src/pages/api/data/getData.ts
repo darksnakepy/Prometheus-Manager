3a4f5291d0b4eaa0833e063dd7b280b329ee2401
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-03 20:27:58 +0200

    Endpoint to get the accounts

---
+++ src/pages/api/data/getData.ts
@@ -0,0 +1,29 @@
+import { prisma } from "~/server/db"
+import type { NextApiRequest, NextApiResponse } from "next";
+import Account from "~/types/Account";
+
+interface DataRequest {
+    sessionId: string
+}
+
+//Get all the accounts
+export default async function handler(request: NextApiRequest, response: NextApiResponse<string>){
+    if (request.method == "POST"){
+        const req = request.body as DataRequest;
+        const user = await prisma.user.findFirst({
+            where: {
+                sessionId: req.sessionId
+            }
+        })
+        if(user) {
+            const data: Account[] = await prisma.account.findMany({
+                where: {
+                    userId: user.id
+                }
+            })
+            
+            response.status(200).json(JSON.stringify(data));
+        }
+    }
+}
+export type { DataRequest }
