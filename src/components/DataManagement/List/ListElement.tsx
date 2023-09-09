68d65cf80265d4434d8daf41037e700fac547b44
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-09 21:03:45 +0200

    Added the function in the components and removed the placeholder image

--- src/components/DataManagement/List/ListElement.tsx
+++ src/components/DataManagement/List/ListElement.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import DisplayDataProps from "~/types/DisplayDataProps";
+import { fetchImage } from "~/utils/FetchImage";
 
 const ListElement = ({date, email, icon, link, type, onClick} :DisplayDataProps) => {
     return (
@@ -7,7 +8,7 @@ const ListElement = ({date, email, icon, link, type, onClick} :DisplayDataProps)
         <div className="w-[68.5%] h-20 ml-[12%] border-2 border-[#1545af] rounded-md flex flex-row relative text-white mb-4 cursor-pointer" onClick={onClick}>
            <div className="flex items-center ml-4">
                 <div className="w-8">
-                        <Image src="/0yha5ybx1qk71.png" width={32} height={32} alt={"icon"} className="rounded-2xl"></Image>
+                        <Image src={fetchImage(link)} width={32} height={32} alt={"icon"}></Image>
                 </div>
            </div>
            <div className="flex flex-col justify-center ml-5">
--- src/components/DataManagement/ViewData/ViewData.tsx
+++ src/components/DataManagement/ViewData/ViewData.tsx
@@ -1,12 +1,16 @@
 import ViewDataProps from "~/types/ViewDataProps";
 import Image from "next/image";
+import { fetchImage } from "~/utils/FetchImage";
 
 const DataView = ({ link, email, password, date, notes, passwordSecurity }: ViewDataProps) => {
+
+
+
   return (
     <div className="flex flex-row justify-center z-15">
       <div className="w-[50%] h-[60%] bg-[#1c1f20] absolute top-[10%] flex-col border-2 border-[#1545af] rounded-2xl ml-auto mr-auto">
         <div className="flex items-center flex-col gap-5 overflow-hidden mt-5 mb-2">
-          <Image src="/0yha5ybx1qk71.png" width={100} height={100} alt="" />
+          <Image src={fetchImage(link)} width={100} height={100} alt="" />
           <h1 className="text-white text-[28px] font-bold mb-5">{link}</h1>
           <div className="w-[65%] flex flex-col text-white space-y-5">
             <div className="label-and-value flex items-center">
