a89a57c1578a0e4ab3cfe028a16ca26d80d7c8d8
darksnakepy <darkhuntshowdown@gmail.com>
2023-09-18 10:52:03 +0200

    modified the logout's image size

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -35,7 +35,7 @@ const NavBar = ({onAccountClick, isLogged, userName} : NavBarProps) =>{
                         {isLogged ? <Link href={"/dashboard"} className="hover:text-gray-300">Dashboard</Link> : <Link href={"/login"} className="hover:text-gray-300">Dashboard</Link>} 
                     </div>
                 </div> 
-                    {isLogged ? <Image className="cursor-pointer ml-8" src={"/exit.svg"} width={30} height={30} alt="logo" onClick={() => {removeCookies("token"); removeCookies("email"); router.reload()}}/>
+                    {isLogged ? <Image className="cursor-pointer ml-8" src={"/exit.svg"} width={25} height={25} alt="logo" onClick={() => {removeCookies("token"); removeCookies("email"); router.reload()}}/>
                         : <button className="ml-8 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-[#1545af] dark:hover:bg-blue-800 focus:outline-none" onClick={onAccountClick}>Sign up</button>}
                 </div>
         </div>
