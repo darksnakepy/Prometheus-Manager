377e9c1dd35ad936ff0b67e6ee58a0a8ef0352f8
darksnakepy <darkhuntshowdown@gmail.com>
2023-12-19 11:40:39 +0100

    queries

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -38,13 +38,13 @@ const LoginForm = () => {
     }
 
     return (
-        <div className="flex justify-center">
-            <div className="w-[90%] sm:w-[25%] h-[60%] bg-[#1c1f20] w-15 absolute top-[20%] flex-col border-2 border-[#1545af] rounded-2xl ml-auto mr-auto flex">
+        <div className="flex justify-center items-center w-full h-screen">
+            <div className="max-w-[400px] w-[100%] h-[500px] bg-[#1c1f20] top-[20%] flex-col border-2 border-[#1545af] rounded-2xl ml-auto mr-auto flex">
                 <div className="flex items-center flex-col gap-25px overflow-hidden">
                     <Image width="80" height="60" alt="Logo" src={Logo} className="mt-3"></Image>
                     <h1 className="text-white text-[24px] font-bold mt-8">LOG IN</h1>
-                    <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} name="username" placeholder="Email" className="outline-none pl-[14px] bg-[#181a1b] mt-5 w-[70%] sm:w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
-                    <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => setMasterPass(e.target.value)} name="masterPass" placeholder="Your master password here" className="outline-none pl-[14px] bg-[#181a1b]  w-[70%] sm:w-[60%] h-12 mb-[3%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
+                    <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} name="username" placeholder="Email" className="outline-none pl-[14px] bg-[#181a1b] mt-5 w-[70%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
+                    <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => setMasterPass(e.target.value)} name="masterPass" placeholder="Your master password here" className="outline-none pl-[14px] bg-[#181a1b] w-[70%] h-12 mb-[3%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
                     <Link className="text-[#1545af] text-[14px] hover:underline" href={""}>Forgot the master password?</Link>
                     <button 
                     className={`w-[70%] sm:w-[60%] h-[15%] mt-[15%] relative text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm flex items-center justify-center dark:bg-[#1545af] dark:hover:bg-blue-800 focus:outline-none`} onClick={submit}>
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -53,21 +53,22 @@ const SignUpForm = () =>{
     }
         
     return(
-        <div className="flex justify-center">
-                    <div className="w-full sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 h-[70%] mx-auto bg-[#1c1f20] w-15 absolute top-[15%] flex-col border-2 border-[#1545af] rounded-2xl ml-auto mr-auto flex">                <div className="flex items-center flex-col gap-25px overflow-hidden">
-                    <Image width="80" height="60" alt="Logo" src={Logo} className="mt-3"></Image> 
-                    <h1 className="text-white text-[24px] font-bold mt-8">SIGN UP</h1>
-                    <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} name="email" placeholder="Email" className="outline-none pl-[14px] bg-[#181a1b] mt-5 w-[70%] sm:w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white" />
-                    <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setMasterPass(e.target.value)} name="masterPass" placeholder="Create a master password" className="outline-none pl-[14px] bg-[#181a1b] mt-1.5 w-[70%] sm:w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white "/>
-                    <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setRepeatPass(e.target.value)} name="phoneNumber" placeholder="Repeat master password " className="outline-none pl-[14px] bg-[#181a1b] mt-1.5 w-[70%] sm:w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
-                    <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setPhoneNumber(e.target.value)} name="email" placeholder="Phone number (optional)" className="outline-none pl-[14px] bg-[#181a1b] mt-1.5 w-[70%] h-12 sm:w-[60%] mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
-                    
-                    <button className="w-[70%] sm:w-[60%] h-[10%] mt-[5%] text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-[#1545af] dark:hover:bg-blue-800 focus:outline-none" onClick={submit}>Register</button>
-                    <div className="text-white mt-3 text-[15px]">
-                        Already a member? <Link className="hover:underline" href={"/login"}>Log in</Link>
-                    </div>
-                    <div className="text-[#b81414]">{error}</div>
-                </div>
+        <div className="flex justify-center items-center w-full h-screen overflow-hidden">
+          <div className="max-w-[450px] w-[90%] h-[550px] bg-[#1c1f20] top-[15%] flex-col border-2 border-[#1545af] rounded-2xl ml-auto mr-auto flex">                
+          <div className="flex items-center flex-col gap-25px overflow-hidden">
+          <Image width="80" height="60" alt="Logo" src={Logo} className="mt-3"></Image> 
+          <h1 className="text-white text-[24px] font-bold mt-6">SIGN UP</h1>
+          <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} name="email" placeholder="Email" className="outline-none pl-[14px] bg-[#181a1b] mt-5 w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white" />
+          <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => setMasterPass(e.target.value)} name="masterPass" placeholder="Create a master password" className="outline-none pl-[14px] bg-[#181a1b] mt-1.5 w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white "/>
+          <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => setRepeatPass(e.target.value)} name="phoneNumber" placeholder="Repeat master password " className="outline-none pl-[14px] bg-[#181a1b] mt-1.5 w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
+          <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setPhoneNumber(e.target.value)} name="email" placeholder="Phone number (optional)" className="outline-none pl-[14px] bg-[#181a1b] mt-1.5 w-[60%] h-12 mb-[5%] rounded-[4px] placeholder:text-white/[0.6] placeholder:text-[14px] text-[14px] text-white " />
+          
+          <button className="w-[60%] h-[10%] mt-[3%] text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-[#1545af] dark:hover:bg-blue-800 focus:outline-none" onClick={submit}>Register</button>
+          <div className="text-white mt-5 mb-3 text-[15px]">
+              Already a member? <Link className="hover:underline" href={"/login"}>Log in</Link>
+          </div>
+          <div className="text-[#b81414]">{error}</div>
+            </div>
             </div>
         </div>
     )
